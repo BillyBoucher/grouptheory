a4b21fcc94c438f9265e715178d0c6e6bdb7f1f4
whopperjr <bellaykang@gmail.com>
2020-06-17 11:58:08 -0700

    warm up exercises

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,11 @@
+import random
 print("Jello")
 print("hello")
 
-#testing now why isn't this working
+#generate random numbers until number generated > .99
+tracker = True
+
+
+while tracker:
+    number = random.random()
+    tracker = number < .99
