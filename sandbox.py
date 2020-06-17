273f9e582ad7ca383a0f8dc0cc3f5ebaea656a7b
whopperjr <bellaykang@gmail.com>
2020-06-17 12:03:31 -0700

    warm up exercises

--- sandbox.py
+++ sandbox.py
@@ -4,9 +4,10 @@ print("hello")
 
 #generate random numbers until number generated > .99
 tracker = True
-
+counter = 0;
 
 while tracker:
     number = random.random()
-    print(number)
+    counter += 1
+    print(str(counter) + ": " + str(number))
     tracker = number < .99
